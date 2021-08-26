b2335aeaf6b7191545b2c388f889bebc87cd2034
Sajanpreet Singh <ssingh17@huskers.unl.edu>
2021-08-26 22:21:13 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Sajanpreet Singh
+ * Date: 2021/08/26
+ *
+ * A simple hello world program in C
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+ int main(int argc, char **argv) {
+
+     printf("Sajanpreet Singh\n");
+
+     return 0;
+ }
